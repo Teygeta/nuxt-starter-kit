cdd4f6435b441cfeaa27ddadaba5b5cc45aee51e
Vittorio <98349901+Teygeta@users.noreply.github.com>
2025-01-01 19:16:33 +0100

    chore: init shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,12 @@ import './env'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss',
-  ],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   build: {
     transpile: ['trpc-nuxt'],
+  },
+  shadcn: {
+    componentDir: './components/shadcn-ui',
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
